c772099760148c44e09118c50330190304d912ea
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-03 15:21:13 +0200

    Jes eli näin se on
    Joo näin se on

--- 03/02.py
+++ 03/02.py
@@ -1,12 +1,13 @@
 import re
 
-with open("rapl.txt") as f:
+with open("input.txt") as f:
     datarivit = f.readlines()
     data = "".join(datarivit)
 
-data_siistitty = re.sub("^(don't\(\)).*(do\(\))$", "\n\nDOUNTTT()\n\n", data)
+data_siistitty = re.sub(r"don\'t\(\).*?do\(\)", "", data, flags=re.DOTALL)
+# Kopioitu: instruction = re.sub(r"don't\(\).*?($|do\(\))", '', instruction, flags=re.DOTALL)
 
-laskut = re.findall("mul\(\d{1,3}\,\d{1,3}\)", data_siistitty)
+laskut = re.findall(r"mul\(\d{1,3}\,\d{1,3}\)", data_siistitty)
 
 summa = 0
 
@@ -17,12 +18,15 @@ for lasku in laskut:
 print(data_siistitty)
 print(summa)
 
+
+
+
     
 # Aiemmalla tuli vastaukseksi 72147484 ja se on liian pieni
 # Nyt tulee 179571322 eli se ei sensuroi mitään
 
 # Manuaalisesti siistimällä tuli 70917760, liian pieni
-Ja sitten taas joka on liian korkea 105264641
-105264641
-178341598
+# Ja sitten taas joka on liian korkea 105264641
+# 105264641
+# 178341598
 
