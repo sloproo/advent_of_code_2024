3579b095ed9cb5062370f2c4ec4114bb7564c80d
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-03 13:18:19 +0200

    Update 02.py
    Töissä säädettyä

---
+++ 03/02.py
@@ -0,0 +1,28 @@
+import re
+
+with open("rapl.txt") as f:
+    datarivit = f.readlines()
+    data = "".join(datarivit)
+
+data_siistitty = re.sub("^(don't\(\)).*(do\(\))$", "\n\nDOUNTTT()\n\n", data)
+
+laskut = re.findall("mul\(\d{1,3}\,\d{1,3}\)", data_siistitty)
+
+summa = 0
+
+for lasku in laskut:
+    kerrottavat = [int(luku) for luku in re.findall("\d{1,3}", lasku)]
+    summa += kerrottavat[0] * kerrottavat[1]
+
+print(data_siistitty)
+print(summa)
+
+    
+# Aiemmalla tuli vastaukseksi 72147484 ja se on liian pieni
+# Nyt tulee 179571322 eli se ei sensuroi mitään
+
+# Manuaalisesti siistimällä tuli 70917760, liian pieni
+Ja sitten taas joka on liian korkea 105264641
+105264641
+178341598
+
