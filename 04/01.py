79a7658dde2de8e1b1ddeb94ed05857d1ba7f0b4
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-04 16:59:43 +0200

    eka

--- 04/01.py
+++ 04/01.py
@@ -7,32 +7,41 @@ def sisalla(x: int, y: int, ruudukko: list) ->bool:
 
 def etsi(x: int, y: int, dx: int, dy: int, sana: str, ruudukko: list):
     if not (sisalla(x+dx, y+dy, ruudukko)):
+        # print(f"Tuli reuna vastaan {x+dx} {y+dy}")
         return 0
     if ruudukko[y+dy][x+dx] == sana[1]:
         if len(sana) == 2:
-            print(f"Löytyi, päätepiste {x+dx}, {y+dy}, suunta {dx}, {dy}")
+            print(f"Sana täyteen")
+            print(f"Alkukohta x = {x-2*dx}, y = {y-2*dy}, suunta {dx}, {dy}")
             return 1
         else:
+            # print(f"Löytyi kirjain {sana[1]}")
             return etsi(x+dx, y+dy, dx, dy, sana[1:], ruudukko)
     else:
+        # print(f"Ei löytynyt, tuli {ruudukko[y+dy][x+dx]}")
         return 0
 
-ruudukko = []
-with open("alku.txt") as f:
-    for r in f:
-        rivi = [kirjain for kirjaimet in r.strip().split() for kirjain in kirjaimet]
-        print(rivi)
-        ruudukko.append(rivi)
+def lue(tiedosto: str) -> list:
+    palautettava =[]
+    with open(tiedosto) as f:
+        for r in f:
+            rivi = [kirjain for kirjaimet in r.strip().split() for kirjain in kirjaimet]
+            palautettava.append(rivi)
+    return palautettava
+
+ruudukko = lue("input.txt")
 
 sana = "XMAS"
 sanoja_loytynyt = 0 
 
-for y in range(len(ruudukko) - 1):
-    for x in range(len(ruudukko[y]) - 1):
+for y in range(len(ruudukko)):
+    for x in range(len(ruudukko[y])):
         if ruudukko[y][x] == sana[0]:
+            # print(f"Löytyi kirjain X kohdasta x = {x}, y = {y}")
             suunnat = [pari for pari in itertools.product([-1, 0, 1], repeat=2)]
             suunnat.remove((0,0))
             for dx, dy in suunnat:
+                # print(f"Etsitään suunnasta dx = {dx}, dy = {dy}")
                 sanoja_loytynyt += etsi(x, y, dx, dy, sana, ruudukko)
             
 
