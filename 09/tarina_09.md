9c789b18252cba5451f87c710bf1d11016dd9a49
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-10 18:17:46 +0200

    eka

--- 09/tarina_09.md
+++ 09/tarina_09.md
@@ -2,7 +2,7 @@
 
 Juuh. Tänään oltiin taas jossain ja jollakulla oli ongelmia tietokoneen kanssa ja minun piti se ratkaista. Meressä äyriäisten kanssa itse asiassa, mutta tämän vuoden tarina on aika irrallinen eikä siksi jää mieleen.
 
-No, tehtävänä oli analysoida tietokoneen kovalevyn sisältö: tyhjä levytila ja tiedostot. Ekassa tehtävässä tyhjää tilaa piti täyttää alusta päin tiedostojen osilla yksi tyhjä blokki kerrallaan. Kakkososassa piti sitten tehdä sama niin, että tiedostot siirretään sellaisiin tyhjiin kohtiin, johon ne mahtuvat kokonaisuudessaan.
+No, tehtävänä oli analysoida tietokoneen kovalevyn sisältö: tyhjä levytila ja tiedostot. Ekassa tehtävässä piti lukea tyhjää tilaa alusta päin ja tiedostojen osia lopusta päin ja siirtää tiedostot tyhjään tilaan blokki kerrallaan. Kakkososassa piti sitten tehdä sama niin, että tiedostot siirretään sellaisiin tyhjiin kohtiin, johon ne mahtuvat kokonaisuudessaan.
 
 Kivasti meni eka osa ja kivasti tuntui menevän tokakin osa, paitsi että vastaus oli väärä. Korjailin koodia, katsoin tuleeko eri vastaus, mutta samaa antoi. Katselin koodia, yritin löytää korjattavaa mutta mitään korjattavaa ei löytynyt, kaikki toimi niin kuin piti, testidatalla homma toimi jne.
 
--- 10/01.py
+++ 10/01.py
@@ -1,15 +1,43 @@
 def avaa(tiedosto: str) -> list:
-  with open(tiedosto) as f:
-    return [merkki for r in f for merkki in r.strip()]
+    with open(tiedosto) as f:
+        avattu = []
+        for r in f:
+            avattu.append([int((m)) for m in r.strip()])
+    return avattu
 
-def sisalla(x: int, y: int, ruudukko: list) -> bool:
-  if x < 0 or y < 0 or y >= len(ruudukko) or x >= len(ruudukko[y]):
-    return False
-  else:
-    return True
+kartta = avaa("input.txt")
 
-def naapurit(x: int, y: int, ruudukko: list) -> list:
-  return [(xn, yn) for xn, yn in [(x, y-1), (x+1, y), (x, y+1), x-1, y)] if sisalla(xn, yn, ruudukko)]
+def sisalla(x: int, y: int, ruudukko: list =kartta) -> bool:
+    if x < 0 or y < 0 or y >= len(ruudukko) or x >= len(ruudukko[y]):
+        return False
+    else:
+        return True
+    
+def naapurit(x: int, y: int, ruudukko: list =kartta) -> list:
+    return [(xn, yn) for xn, yn in [(x, y-1), (x+1, y), (x, y+1), (x-1, y)] if sisalla(xn, yn, ruudukko)]
 
-kartta = avaa("alku.txt")
+
+nollat = []
+for y in range(len(kartta)):
+    for x in range(len(kartta[y])):
+        if kartta[y][x] == 0:
+            nollat.append((x, y))
+mahdolliset = {0: nollat}
+
+reitteja = 0
+
+for x, y in nollat:
+    nykyiset = [(x, y)]
+    for taso in range(9):
+        seuraavat = []
+        for xn, yn in nykyiset:
+            for xs, ys in naapurit(xn, yn):
+                if kartta[ys][xs] == taso + 1:
+                    seuraavat.append((xs, ys))
+        seuraavat = list(set(seuraavat))
+        nykyiset = seuraavat.copy()
+    reitteja += len(seuraavat)
+
+
+print(reitteja)
 
