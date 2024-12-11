9899c03763eb14974c8552c8eaf5ceceba6e2357
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-11 10:37:00 +0200

    Eka läpi

--- 11/01.py
+++ 11/01.py
@@ -1,8 +1,7 @@
-import math
-
 def avaa(tiedosto: str) -> list:
     with open(tiedosto) as f:
-        return f.readline().strip().split(" ")
+        # raaka = f.readline().strip().split(" ")
+        return [int(kivi) for kivi in f.readline().strip().split(" ")]
 
 def litista(lista: list) -> list:
     return [kohde for kohde in lista for kohde in lista]
@@ -11,10 +10,34 @@ def nollasta(kivi: int) -> int:
     assert kivi == 0
     return 1
 
-def pari_log(kivi: int) -> tuple[int, int]:
-    assert len(str(kivi)) % 0
+def parill_pituus(kivi: int) -> list:
+    kivi = str(kivi)
+    assert len(kivi) % 2 == 0
+    eka = kivi[:len(kivi) // 2]
+    toka = kivi[len(kivi) // 2:]
+    return [int(eka), int(toka)]
+
+def jama(kivi:int) -> int:
+    return kivi * 2024
+
+def kierros(kivet: list) -> list:
+    seuraava = []
+    for kivi in kivet:
+        if kivi == 0:
+            seuraava.append(nollasta(kivi))
+        elif len(str(kivi)) % 2 == 0:
+            seuraava += parill_pituus(kivi)
+        else:
+            seuraava.append(jama(kivi))
+    return seuraava
+
     
 
 
 
-print(avaa("alku.txt"))
+kivet = avaa("input.txt")
+print(kivet)
+for i in range(25):
+    kivet = kierros(kivet)
+    print(len(kivet))
+    
