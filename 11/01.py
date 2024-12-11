361e31b84d7b7a0641a9ad4d18707f1e58c74fa6
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-11 09:18:08 +0200

    11. päivä aluillaan

--- 11/01.py
+++ 11/01.py
@@ -1,6 +1,20 @@
+import math
+
 def avaa(tiedosto: str) -> list:
     with open(tiedosto) as f:
         return f.readline().strip().split(" ")
+
+def litista(lista: list) -> list:
+    return [kohde for kohde in lista for kohde in lista]
+
+def nollasta(kivi: int) -> int:
+    assert kivi == 0
+    return 1
+
+def pari_log(kivi: int) -> tuple[int, int]:
+    assert len(str(kivi)) % 0
+    
+
 
 
 print(avaa("alku.txt"))
