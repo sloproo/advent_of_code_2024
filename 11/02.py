d9ce45815af31018e47c7d0bf4828777edae1ed8
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-11 10:46:50 +0200

    Kakkonen ideatasolla varmaan ratkaistu
    Jes

--- 11/02.py
+++ 11/02.py
@@ -1,3 +1,6 @@
+# tässä tehtävässä kivijonoon tulee uusilla kierroksilla uusia 2024:sia ja ne kehittyvät seuraavilla kierroksilla säännönmukaisesti.
+# Samalla tavalla mekaanisen ennaltasäädetysti kehittyy 1.
+
 def avaa(tiedosto: str) -> list:
     with open(tiedosto) as f:
         return f.readline().strip().split(" ")
