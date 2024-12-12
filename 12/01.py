a34088c730664dd790ffcc11ec329f4b0ec4b23d
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-12 07:30:10 +0200

    alkuun

---
+++ 12/01.py
@@ -0,0 +1,9 @@
+def avaa(tiedosto: str) -> list:
+    kartta = []
+    with open(tiedosto) as f:
+        for r in f:
+            kartta.append([ruutu for ruutu in r.strip()])
+    return kartta
+
+kartta = avaa("alku.txt")
+print(kartta)
