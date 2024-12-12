60da34dc081409ed5572421ed82c9892aa065057
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-12 23:03:59 +0200

    Toka läpi
    Tämän koodailut tosiaan aloitettiin Hulkkosen levyn ennakkokuuntelun jälkeen Bar Ullakolla
    Siinä meni eka aika lailla maaliin ja kotona viimeistelin.
    
    Tokaan osioon sit kun keksi, että haetaan sivun loppua eli siis kulmaa, niin homma ratkesi kivasti.
    Olen iloinen

--- 12/01.py
+++ 12/01.py
@@ -5,45 +5,54 @@ def avaa(tiedosto: str) -> list:
             kartta.append([ruutu for ruutu in r.strip()])
     return kartta
 
-kartta = avaa("alku.txt")
+kartta = avaa("input.txt")
 kaytetyt = []
 
-def sisalla(x: int, y: int, ruudukko: list =kartta) ->bool:
+def sisalla(x: int, y: int, ruudukko: list =kartta) -> bool:
     if x < 0 or y < 0 or y >= len(ruudukko) or x >= len(ruudukko[y]):
         return False
     return True
 
 def naapurit(x: int, y: int, ruudukko: list =kartta) -> list:
-    return [(xn, yn) for xn, yn in [(x, y-1), (x+1, y), (x, y+1), (x-1, y)] if sisalla(xn, yn, ruudukko)]
+    return [(xn, yn) for xn, yn in [(x, y-1), (x+1, y), (x, y+1), (x-1, y)]]
 
-def tee_alue(x: int, y: int, kartta: list, kaytetyt: list) -> list:
-    if (x, y) in kaytetyt:
-        return [[-1], -1]
-    else:
-        kirjain = kartta[y][x]
-        alue = [(x, y)]
-        vieraat_naapurit = []
-        kasvoi = True
-        while kasvoi:
-            kasvoi = False
-            for xa, ya in alue:
-                for (xn, yn) in naapurit(xa, ya, kartta):
-                    if kartta[yn][xn] == kirjain and (xn, yn) not in kaytetyt and (xn, yn) not in alue:
-                        kasvoi = True
-                        alue.append((xn, yn))
-                    elif kartta[yn][xn] not in vieraat_naapurit:
-                        vieraat_naapurit.append((xn, yn))
-                        
-        return (alue, len(vieraat_naapurit))
+def tee_alue(x: int, y: int, kartta: list) -> tuple[list, int]:
+    kirjain = kartta[y][x]
+    alue = []
+    uudet = [(x, y)]
+    tulevat = []
+    raja_aitaa = 0
+    kasvoi = True
+    while kasvoi:
+        kasvoi = False
+        for xa, ya in uudet:
+            for (xn, yn) in naapurit(xa, ya, kartta):
+                if not sisalla(xn, yn, kartta):
+                    raja_aitaa += 1
+                elif kartta[yn][xn] != kirjain:
+                    raja_aitaa += 1
+                elif (xn, yn) not in uudet + alue + tulevat:
+                    kasvoi = True
+                    tulevat.append((xn, yn))
+        alue += [koord for koord in uudet]
+        uudet = [koord for koord in tulevat]
+        tulevat = []
+
+    return (alue, raja_aitaa)
 
 alueet = []
 kaytetyt = []
 
 for y in range(len(kartta)):
     for x in range(len(kartta[y])):
-        uusi_alue = tee_alue(x, y, kartta, kaytetyt)
-        if uusi_alue != ([-1], -1):
-            alueet.append(uusi_alue)
-            kaytetyt += uusi_alue[0]
+        if (x, y) in kaytetyt:
+            continue
 
-print(alueet)
+        uusi_alue = tee_alue(x, y, kartta)
+        alueet.append(uusi_alue)
+        kaytetyt += uusi_alue[0]
+        pass
+
+hinnat = [len(alue[0]) * alue[1] for alue in alueet]
+print(sum(hinnat))
+
--- 12/02.py
+++ 12/02.py
@@ -5,7 +5,77 @@ def avaa(tiedosto: str) -> list:
             kartta.append([ruutu for ruutu in r.strip()])
     return kartta
 
-kartta = avaa("alku.txt")
-print(kartta)
+kartta = avaa("input.txt")
+kaytetyt = []
 
+def sisalla(x: int, y: int, ruudukko: list =kartta) -> bool:
+    if x < 0 or y < 0 or y >= len(ruudukko) or x >= len(ruudukko[y]):
+        return False
+    return True
 
+def naapurit(x: int, y: int, ruudukko: list =kartta) -> list:
+    return [(xn, yn) for xn, yn in [(x, y-1), (x+1, y), (x, y+1), (x-1, y)]]
+
+def tee_alue(x: int, y: int, kartta: list) -> list:
+    kirjain = kartta[y][x]
+    alue = []
+    uudet = [(x, y)]
+    tulevat = []
+    kasvoi = True
+    while kasvoi:
+        kasvoi = False
+        for xa, ya in uudet:
+            for (xn, yn) in naapurit(xa, ya, kartta):
+                if not sisalla(xn, yn, kartta):
+                    continue
+                elif kartta[yn][xn] != kirjain:
+                    continue
+                elif (xn, yn) not in uudet + alue + tulevat:
+                    kasvoi = True
+                    tulevat.append((xn, yn))
+        alue += [koord for koord in uudet]
+        uudet = [koord for koord in tulevat]
+        tulevat = []
+    return (alue)
+
+def reunat_kulmista(alue: list) -> int:
+    reunoja = 0
+    for x, y in alue:
+        if (x-1, y) not in alue and ((x, y+1) not in alue or (x-1, y+1) in alue):
+            reunoja += 1
+        if (x+1, y) not in alue and ((x, y+1) not in alue or (x+1, y+1) in alue):
+            reunoja += 1
+        if (x, y-1) not in alue and ((x+1, y) not in alue or (x+1, y-1) in alue):
+            reunoja += 1
+        if (x, y+1) not in alue and ((x+1, y) not in alue or (x+1, y+1) in alue):
+            reunoja += 1
+    return reunoja
+
+"""
+Vasen reuna: vasemmalla ei, joko alhaalla ei tai alhaalla vasemmalla on 
+Oikea reuna: oikealla ei, joko alhaalla ei tai alhaalla oikealla on
+Yläreuna: ylhäällä ei, joko oikealla ei ole tai ylhäällä oikealla on
+Alareuna: alhaalla ei, joko oikealla ei ole tai alhaalla oikealla on
+"""
+
+alueet = []
+kaytetyt = []
+
+for y in range(len(kartta)):
+    for x in range(len(kartta[y])):
+        if (x, y) in kaytetyt:
+            continue
+
+        uusi_alue = tee_alue(x, y, kartta)
+        alueet.append(uusi_alue)
+        kaytetyt += uusi_alue
+        pass
+
+hinnat = sum([len(alue) * reunat_kulmista(alue) for alue in alueet])
+print(hinnat)
+
+# for alue in alueet:
+#     reunoja = reunat_kulmista(alue)
+#     print(f"alueella kokoa {len(alue)}, reunoja {reunoja}")
+#     print(f"Hinta: {len(alue) * reunoja}")
+    
