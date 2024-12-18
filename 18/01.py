8d3519d51df73a59980a5c299f018ab21864c78b
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-18 16:35:36 +0200

    Eka läpi
    Nyt on luokkien käyttö tullut mukaan kuvioihin isosti. Onko tämä näppärämpää näin vai ei - en tiedä.
    
    Aikakirjoihin merkittäköön myös, että olin tänään suuhygienistillä ja siinä hammaskiven irrottelun
    tapahtuessa mietin kahteenkin edelliseen tehtävään vastausta. Se oli jotenkin puoliksi unenkaltainen
    tai dissosiatiivinen tila, joten ihan tarkalleen en enää niihin ajatuksiin pääse käsiksi. Mutta
    pointti oli joka tapauksessa se, että porojen tilat on varmaan ihan siinä itsessään jo ratkaistu
    ja idea valmiina.
    
    Eilisen tietokoneen suhteen sit mietin backtrackaamista ja sitä, kuinka ohjelma kuitenkin tuottaa
    palautteeseen aina yhden numeron kerrallaan, sotkettava A jää jonkinlaiseksi ja sillä käydään
    taas sama komentojono uudestaan läpi. Pitää katsoa sitäkin. Mitä ennen tietenkin tämän tehtävän
    kakkososa...

---
+++ 18/01.py
@@ -0,0 +1,57 @@
+class Kartta:
+    def __init__(self, tiedosto: str):
+        with open(tiedosto) as f:
+            self.bitit = [(int(jaettu[0]), int(jaettu[1])) for jaettu in [r.strip().split(",") for r in f.readlines()]]
+            self.leveys = max(luku for luku in{pari[0] for pari in self.bitit}.union({pari[0] for pari in self.bitit}))
+            self.korruptoituneet = []
+            self.kierros = 0
+            self.alku = (0, 0)
+            self.maali = (self.leveys, self.leveys)
+            self.kaydyt = {}
+            self.saavutetut_ajassa = {}
+
+    def sisalla(self, pari: tuple[int, int]) -> bool:
+        x, y = pari
+        if x < 0 or y < 0 or x > self.leveys or y > self.leveys:
+            return False
+        else:
+            return True
+    
+    def naapurit(self, ruutu: tuple[int, int]) -> list[tuple[int, int]]:
+        x, y = ruutu
+        return {pari for pari in {(x-1, y), (x, y-1), (x+1, y), (x, y+1)} 
+                if self.sisalla(pari)}
+    
+    def luo_tarjokkaat(self, ruutu: tuple[int, int]) -> list[tuple[int, int]]:
+        return [naapuri for naapuri in self.naapurit(ruutu) 
+                if naapuri not in self.korruptoituneet]
+
+    def alusta(self):
+        self.tarjokkaat = {self.alku: self.kierros}
+
+    def kay_kierros(self):
+        seuraavat = []        
+        self.saavutetut_ajassa[self.kierros] = []
+        for tarjokas in self.tarjokkaat:
+            if tarjokas not in self.kaydyt and tarjokas not in self.korruptoituneet:
+                self.kaydyt[tarjokas] = self.kierros
+                self.saavutetut_ajassa[self.kierros] += tarjokas
+                seuraavat += self.luo_tarjokkaat(tarjokas)
+        self.tarjokkaat = list(set(seuraavat))
+        self.kierros += 1
+    
+    def mene_maaliin(self):
+        self.alusta()
+        while self.maali not in self.kaydyt:
+            self.kay_kierros()
+        print(f"Maalissa {self.maali} ollaan ajassa {self.kaydyt[self.maali]}")
+        
+
+kartta = Kartta("input.txt")
+kartta.korruptoituneet = kartta.bitit[:1024]
+kartta.mene_maaliin()
+
+    
+
+
+
