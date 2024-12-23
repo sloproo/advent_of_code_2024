f6257b161cc8ce2f2048c583cd6b2c6d8fdff5fc
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-23 14:45:24 +0200

    Kuinka upeaa: Läpi meni.
    Työlästä oli ja argumenttien purkaminen ja settien puhdistamisen kanssa sai vähän säätää mutta jumalauta: läpi!

--- 23/02.py
+++ 23/02.py
@@ -9,16 +9,50 @@ def lue(tiedosto: str) -> dict:
         for r in f:
             eka, toka = r.strip().split("-")
             if eka in koneet:
-                koneet[eka].append(toka)
+                koneet[eka].add(toka)
             else:
-                koneet[eka] = [toka]
+                koneet[eka] = {toka}
             if toka in koneet:
-                koneet[toka].append(eka)
+                koneet[toka].add(eka)
             else:
-                koneet[toka] = [eka]
+                koneet[toka] = {eka}
         return koneet
+
+def puhdista(joukko: set) -> set:
+    joukko_lyhennetty = {tuple(sorted(jasen)) for jasen in joukko}
+    return joukko_lyhennetty
+
+def kasvata(monikot: set) -> set:
+    suurempi = set()
+    for kopla in monikot:
+        yhteiset = koneet[kopla[0]].intersection(*(set(koneet[muu]) for muu in kopla[1:]))
+        for yhteinen in yhteiset:
+            suurempi.add(kopla + (yhteinen, ))
+    suurempi = puhdista(suurempi)
+    return suurempi
+
     
 koneet = lue("input.txt")
+suurin_mahdollinen = max({len(yhteydet) for yhteydet in koneet.values()})
+suurin_porukka = 0
+
+parit = set()
+for kone in koneet:
+    for kone_2 in koneet[kone]:
+        parit.add((kone, kone_2))
+parit = puhdista(parit)
+
+pass
+joukot = {2: parit}
+
+for i in range(3, suurin_mahdollinen + 1):
+    joukot[i] = kasvata(joukot[i-1])
+
+pisin = joukot[max(joukot.keys())]
+
+vastaus = ",".join(list(pisin)[0])
+print(vastaus)
 
 
+print(f"Aikaa meni {time.time() - alkuaika} sek.")
 
