5895022c9ea6065b4fb341298790d45db850a884
sloproo <3440817+sloproo@users.noreply.github.com>
2024-12-30 20:14:31 +0200

    Joo sisennyksien takia kikkailen.
    Nanolla menee välilyönnit ja tabin painallukset sekaisin

--- 22/02.py
+++ 22/02.py
@@ -6,7 +6,7 @@ def lue(tiedosto :str) -> list:
     with open(tiedosto) as f:
         return [int(r) for r in f.readlines()]
 
-apinoiden_alkuluvut = lue("input.txt")
+apinoiden_alkuluvut = lue("alku2.txt")
 
 def kierros(luku: int) -> int:
     pitka = 16777216
@@ -14,18 +14,19 @@ def kierros(luku: int) -> int:
     b = (a // 32 ^ a) % pitka
     c = (b * 2048 ^ b) % pitka
     return c
-    
+
 sarjalla_rahaa = {}
+i = 0
 
 for luku in apinoiden_alkuluvut:
     apinan_sarjat = {}
-    apinan_hinnat = [luku % 10]
+    apinan_hinnat = [(luku % 10, 0)]
     for i in range(2000):
         luku = kierros(luku)
-        apinan_hinnat.append((luku % 10, luku % 10 - apinan_hinnat[-1]))
-        
+        apinan_hinnat.append((luku % 10, luku % 10 - apinan_hinnat[-1][0]))
+
         if i >= 3:
-            sarja = (tuple([apinan_hinnat[x][1] for x in range(-4, 0)])
+            sarja = tuple([apinan_hinnat[x][1] for x in range(-4, 0)])
             if sarja not in apinan_sarjat:
                 apinan_sarjat[sarja] = apinan_hinnat[-1][0]
     for sarja in apinan_sarjat:
@@ -33,14 +34,15 @@ for luku in apinoiden_alkuluvut:
             sarjalla_rahaa[sarja] = apinan_sarjat[sarja]
         else:
             sarjalla_rahaa[sarja] += apinan_sarjat[sarja]
+        i += 1
+        if i % 200 == 0:
+            print(f"Se oli apina nro {i}")
+            print(f"Aikaa kulunut {time.time() - alkuaika} sekuntia")
 
 print(f"Apinojen hinnankehitykset ja sarjoilla saatavat rahat laskettu")
 print(f"Aikaa kului {time.time() - alkuaika}")
 
-print(f"Paras saalis on {max(sarjalla_rahaa.values())}
+print(f"Paras saalis on {max(sarjalla_rahaa.values())}")
 print(f"Aikaa kului {time.time() - alkuaika}")
 
 pass
-
-
-
